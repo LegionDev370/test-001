1afd4c7e2b019f15c642f2ade3bc961cebe86358
LegionDev370 <llegion630@gmail.com>
2024-11-08 19:51:09 +0500

    file o'zgartirildi

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Hello World");
 console.log("12345");
-console.log("hello doss")
+console.log("hello doss");
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
